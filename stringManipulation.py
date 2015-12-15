d876a85f71cca3089632ea86c5a1f3e101e2208f
Athanasios Garyfalos <thanos.egw@gmail.com>
2015-12-15 12:04:53 +0100

    Updating files with new methods trying to put everything together

--- stringManipulation.py
+++ stringManipulation.py
@@ -18,6 +18,9 @@ class StringManipulationProcess(object):
                  bash_cmd_return=None,
                  android_manifest_file=None):
 
+        """
+        :rtype: self.replace_string (True, False)
+        """
         self.output = output
         self.strings_file = strings_file
         self.regex_string = regex_string
@@ -27,59 +30,76 @@ class StringManipulationProcess(object):
         self.android_manifest_file = android_manifest_file
 
     def get_android_manifest_xml(self, app_section):
+        """
+        :type app_section: Each directory has a different section name to use for each clone
+        """
         working_directory = os.getcwd()
-
         # Create path for 'AndroidManifest.xml' file based on directory
         manifest_xml = working_directory + "/" + app_section + "/app/src/main/AndroidManifest.xml"
-
         self.android_manifest_file = manifest_xml
         return self.android_manifest_file
 
     def execute_bash_cmd(self, cmd):
+        """
+        :type cmd: Command to execute on terminal
+        """
         result = subprocess.check_output(cmd, shell=True)
         self.bash_cmd_return = result
         return self.bash_cmd_return
 
     def get_strings_xml(self, app_section):
+        """
+        :type app_section: Each directory has a different section name to use for each clone
+        """
         working_directory = os.getcwd()
-
         # Search for 'strings.xml' file
         find_strings = "find " + working_directory + "/" + app_section + "/ -name 'strings.xml'"
         strings_xml_path = self.execute_bash_cmd(find_strings)
-
         # Strip new line character
         strings_xml_path = strings_xml_path.rstrip('\r\n')
-
         self.strings_file = strings_xml_path
         return self.strings_file
 
     def get_build_gradle_app(self, app_section):
+        """
+        :param app_section: Each directory has a different section name to use for each clone
+        :rtype: Path to the specified builde.gradle file based on section of configuration.ini file
+        """
         working_directory = os.getcwd()
-
-        # Create path for 'AndroidManifest.xml' file based on directory
+        # Create path for 'build.gradle' file based on directory
         build_gradle = working_directory + "/" + app_section + "/app/build.gradle"
+        self.build_gradle = build_gradle
+        return self.build_gradle
 
-        self.android_manifest_file = build_gradle
-        return self.android_manifest_file
-
-    def str_regex(self, file_input, str_old, str_new):
+    def str_regex(self, file_input, regex_key):
         # Open file and get matching string based on regex
+        """
+        :param regex_key: Find all occurrences based on regex input
+        :param file_input: File input to use regex
+        :rtype: self.regex_string returns string matched based on regex
+        """
+        print "File Input: {}\nRegex Key: {}" .format(file_input, regex_key)
         with open(file_input, "r") as fr:
             str_data = fr.read()
-            str_line = re.findall(r'{}'.format(str_old), str_data)
+            str_line = re.findall(r'{}'.format(regex_key), str_data)
         fr.closed
-        print "XML: {}".format(str_line)
-
         self.regex_string = str_line[0]
+        print "Regex String: {}" .format(self.regex_string)
         return self.regex_string
 
     def str_replace(self, file_input, str_old, str_new):
         # Replace all occurrences of string where is matched
+        """
+        :param str_new: Replace all occurrences found from str_old string
+        :param str_old: Find all occurrences based on input string
+        :type file_input: File input to use to replace string
+        """
         for line in fileinput.input(file_input, inplace=True):
-            line = re.sub(str_old, str_new, line.rstrip())
-            print(line)
+            if line:
+                line = re.sub(str_old, str_new, line.rstrip())
+                print(line)
+                self.replace_string = True
+            else:
+                self.replace_string = False
         fileinput.close()
-        print "File Input: {}\nStr Old: {}\Str New: {}" .format(file_input, str_old, str_new)
-
-        self.replace_string = line
         return self.replace_string
